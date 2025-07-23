6aaf7770458173b8347267ba5d04734d5db6ec95
Stuartferguson00 <99267485+Stuartferguson00@users.noreply.github.com>
2025-07-23 12:43:52 +0100

    fix init

--- src/qemcmc/__init__.py
+++ src/qemcmc/__init__.py
@@ -1,3 +1,15 @@
 def main() -> None:
     print("Hello from qemcmc!")
 
+__all__ = ["MCMCState", "MCMCChain", "plot_chains", "get_random_state", "test_accept","IsingEnergyFunction","CircuitMaker","QeMCMC","MCMC","ModelMaker"]
+
+from .helpers import MCMCState, MCMCChain, plot_chains, get_random_state
+from .energy_models import IsingEnergyFunction
+from .CircuitMaker import CircuitMaker
+from .QeMCMC_ import QeMCMC
+from .MCMC import MCMC
+from .ModelMaker import ModelMaker
+
+
+
+
