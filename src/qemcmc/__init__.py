b5e5fa3da8a7161fd6c96266ff673c1de79c1681
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-10-25 03:16:24 +0100

    fix: updated class imports and global helpers

--- src/qemcmc/__init__.py
+++ src/qemcmc/__init__.py
@@ -1,15 +1,19 @@
-def main() -> None:
-    print("Hello from qemcmc!")
 
-__all__ = ["MCMCState", "MCMCChain", "plot_chains", "get_random_state", "test_accept","IsingEnergyFunction","CircuitMaker","QeMCMC","MCMC","ModelMaker"]
+__version__ = "0.3.32"
 
-from .helpers import MCMCState, MCMCChain, plot_chains, get_random_state
-from .energy_models import IsingEnergyFunction
-from .CircuitMaker import CircuitMaker
+__all__ = ["EnergyModel", "IsingEnergyFunction", "CircuitMaker", "CircuitMakerIsing", "MCMC", "ClassicalMCMC", "QeMCMC",
+            "MCMCState", "MCMCChain", "ModelMaker", "plot_chains", "get_random_state", "get_all_possible_states", "SpectralGap"]
+
+
+from .helpers import MCMCState, MCMCChain, plot_chains, get_random_state, get_all_possible_states
+from .energy_models import EnergyModel, IsingEnergyFunction 
+from .CircuitMaker import CircuitMaker, CircuitMakerIsing
+from .ClassicalMCMC import ClassicalMCMC
 from .QeMCMC_ import QeMCMC
 from .MCMC import MCMC
 from .ModelMaker import ModelMaker
 
 
-
+def main() -> None:
+    print("Hello from qemcmc!")
 
