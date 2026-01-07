ffd7cc8dc940bd198d91a0db1f3091fa1f20de46
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2026-01-07 12:27:35 +0000

    refactor: clean up imports

--- src/qemcmc/MCMC.py
+++ src/qemcmc/MCMC.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Optional
 from tqdm import tqdm
 from .helpers import MCMCChain, MCMCState, get_random_state
-from .energy_models import IsingEnergyFunction, EnergyModel
+from .energy_models import EnergyModel
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
--- src/qemcmc/main.py
+++ src/qemcmc/main.py
@@ -6,57 +6,70 @@ from matplotlib import pyplot as plt
 from joblib import Parallel, delayed
 
 if __name__ == "__main__":
-    
-    
-    
-    def plot_chains(chains: list[MCMCChain], color: str, label:str):
+
+    def plot_chains(chains: list[MCMCChain], color: str, label: str):
         for chain in chains:
             energies = chain.get_current_energy_array()
             pos = chain.get_pos_array()
-            plt.plot(pos, energies, color=color, alpha = 0.3)
-        avg_energy = sum(chain.get_current_energy_array() for chain in chains) / len(chains)
+            plt.plot(pos, energies, color=color, alpha=0.3)
+        avg_energy = sum(chain.get_current_energy_array() for chain in chains) / len(
+            chains
+        )
         plt.plot(pos, avg_energy, color=color, label=f"Average {label}")
-    
-    
+
     n = 7
     steps = 50
     reps = 10
     model_type = "Fully Connected Ising"
     name = "Test Ising model"
-    
+
     model = ModelMaker(n, model_type, name).model
-    
+
     temp = 0.1
     initial_states = model.initial_state
-    
+
     uni_chains = []
     for rep in range(reps):
-        classical_uniform_MCMC = ClassicalMCMC(model, temp, method = "uniform")
-        uni_chain = classical_uniform_MCMC.run(steps, initial_state = initial_states[rep], name = "classical uniform MCMC", verbose = True, sample_frequency = 1)
+        classical_uniform_MCMC = ClassicalMCMC(model, temp, method="uniform")
+        uni_chain = classical_uniform_MCMC.run(
+            steps,
+            initial_state=initial_states[rep],
+            name="classical uniform MCMC",
+            verbose=True,
+            sample_frequency=1,
+        )
         uni_chains.append(uni_chain)
     plot_chains(uni_chains, "orange", "classical uniform MCMC")
-    
+
     loc_chains = []
     for rep in range(reps):
-        classical_local_MCMC = ClassicalMCMC(model, temp, method = "local")
-        loc_chain = classical_local_MCMC.run(steps, initial_state = initial_states[rep], name = "classical local MCMC", verbose = True, sample_frequency = 1)
+        classical_local_MCMC = ClassicalMCMC(model, temp, method="local")
+        loc_chain = classical_local_MCMC.run(
+            steps,
+            initial_state=initial_states[rep],
+            name="classical local MCMC",
+            verbose=True,
+            sample_frequency=1,
+        )
         loc_chains.append(loc_chain)
     plot_chains(loc_chains, "lightgreen", "classical local MCMC")
-    
+
     def run_qemcmc(rep):
         qemcmc = QeMCMC(model, gamma=(0.3, 0.6), time=(2, 20), temp=temp)
-        return qemcmc.run(steps, initial_state=initial_states[rep], name="QeMCMC", verbose=True, sample_frequency=1)
+        return qemcmc.run(
+            steps,
+            initial_state=initial_states[rep],
+            name="QeMCMC",
+            verbose=True,
+            sample_frequency=1,
+        )
 
     Qe_chains = Parallel(n_jobs=-1)(delayed(run_qemcmc)(rep) for rep in range(reps))
     plot_chains(Qe_chains, "lightblue", "QeMCMC")
-    
+
     plt.xlabel("MCMC step")
     plt.ylabel("Energy")
     plt.title("MCMC chains")
     plt.legend()
     plt.show()
-    #plt.savefig("plots/MCMC_chains.png")
-    
-    
-        
-
+    # plt.savefig("plots/MCMC_chains.png")
