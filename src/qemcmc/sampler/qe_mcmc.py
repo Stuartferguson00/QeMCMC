8ff7ef561b3d2dc05ce65dfa86e2fe59bd397bf3
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2026-02-18 19:09:06 +0000

    refactor: initialise CM and coarse grainer in constructor, update get_s_prime logic
    Instead of performing subgroup selection and couplings calculation in CircuitMaker we perform this calculation directly inside get_s_prime as there is nothing quantum about it for it to exist inside the CircuitMaker class

--- src/qemcmc/sampler/qe_mcmc.py
+++ src/qemcmc/sampler/qe_mcmc.py
@@ -2,9 +2,11 @@
 from qemcmc.sampler import MCMC
 from qemcmc.model import EnergyModel
 from qemcmc.circuits import PennyLaneCircuitMaker
+from qemcmc.coarse_grain import CoarseGraining
 
 # External package imports
 import warnings
+import numpy as np
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
@@ -21,6 +23,7 @@ class QeMCMC(MCMC):
         time: int | tuple[int],
         temp: float,
         delta_time: float = 0.8,
+        coarse_graining=None,
     ):
         """
         Initializes an instance of the QeMCMC class.
@@ -43,14 +46,12 @@ class QeMCMC(MCMC):
         self.update = self.get_s_prime
         self.method = "quantum"
 
-        # Initialize quantum circuit here
-        # Uncomment either 1. or 2. depending on which CM you want to use.
-
         # 1. Use Qiskit CircuitMaker
         # self.CM = CircuitMaker(self.model, self.gamma, self.time)
 
         # 2. Use Pennylane CircuitMaker
         self.CM = PennyLaneCircuitMaker(self.model, self.gamma, self.time)
+        self.cg = coarse_graining or CoarseGraining(model.n)  # defaults to full system
 
     def get_s_prime(self, current_state: str) -> str:
         """
@@ -62,13 +63,24 @@ class QeMCMC(MCMC):
         Returns:
         str: The next state s_prime.
         """
+        g = self.gamma
+        t = self.time
+        if isinstance(self.gamma, tuple):
+            g = np.random.uniform(min(self.gamma), max(self.gamma))
+        if isinstance(self.time, tuple):
+            t = np.random.randint(min(self.time), max(self.time) + 1)
 
-        # Uncomment either 1. or 2. depending on which CM you want to use and if you want to do CG.
+        subgroup_choice = self.cg.sample()
+        local_couplings = self.model.get_subgroup_couplings(subgroup=subgroup_choice, current_state=current_state)
+
+        self.CM.gamma = g
+        self.CM.time = t
+        self.CM.local_couplings = local_couplings
 
         # 1. Get s_prime using generic CircuitMaker
         # s_prime = self.CM.get_state(current_state)
 
         # 2. Get s_prime for coarse graining
-        s_prime = self.CM.update(current_state)
+        s_prime = self.CM.update(s=current_state, subgroup_choice=subgroup_choice)
 
         return s_prime
