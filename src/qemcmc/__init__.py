6265f3f28bb162a126785da6322deab4673b4414
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2026-02-27 14:40:01 +0000

    feat: add SepctralGap class

--- src/qemcmc/__init__.py
+++ src/qemcmc/__init__.py
@@ -2,7 +2,7 @@ from .sampler import MCMC, ClassicalMCMC, QeMCMC
 from .model import EnergyModel
 from .circuits import CircuitMaker, PennyLaneCircuitMaker
 from .utils import MCMCState, MCMCChain, plot_chains, get_random_state, get_all_possible_states, ModelMaker
-
+from .spectralgap import SpectralGap
 
 __all__ = [
     "EnergyModel",
