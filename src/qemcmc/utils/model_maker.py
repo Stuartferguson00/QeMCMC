cd453d51b1642751bebb1cb1937e367fab3c3ddb
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2026-02-27 14:41:54 +0000

    feat: add fully connected ising and coarse grained ising

--- src/qemcmc/utils/model_maker.py
+++ src/qemcmc/utils/model_maker.py
@@ -4,27 +4,30 @@ from qemcmc.coarse_grain import CoarseGraining
 
 # External package imports
 import numpy as np
+import itertools
 
 
 class ModelMaker:
     # Class to control the initialisation of an energy model.
     # It might seem a bit convoluted, but will allow for more complex models to be made in future.
-    def __init__(self, n_spins: int, model_type: str, name: str, J: np.ndarray = None, h: np.ndarray = None, cost_function_signs: list = [-1, -1]):
+    def __init__(self, n_spins: int, model_type: str, name: str, coarse_graining_number: int = 1, cost_function_signs: list = [-1, -1]):
         self.name = name
         self.n_spins = n_spins
-        self.cost_function_signs = cost_function_signs
-        if type(model_type) is not str:
-            print("model type must be a string representing the model you request")
-        elif model_type == "Fully Connected Ising":
-            self.make_fully_connected_Ising()
-        elif model_type == "Fully Connected Ising Generic":
-            self.make_fully_connected_Ising_generic()
+        self.cost_function_signs = cost_function_signs or [-1, -1]
+
+        if not isinstance(model_type, str):
+            raise TypeError("model_type must be a string")
+
+        if model_type == "Fully Connected Ising":
+            self.make_fully_connected_ising()
         elif model_type == "Coarse Grained Ising":
-            self.make_coarse_grained_ising()
+            self.make_coarse_grained_ising(coarse_graining_number)
         elif model_type == "1D Ising":
             self.make_1D_Ising()
+        else:
+            raise ValueError(f"Unknown model_type: {model_type}")
 
-    def make_fully_connected_Ising_generic(self):
+    def make_fully_connected_ising(self):
         shape_of_J = (self.n_spins, self.n_spins)
         J = np.round(np.random.normal(0, 1, shape_of_J), decimals=4)
         J_tril = np.tril(J, -1)
@@ -34,10 +37,9 @@ class ModelMaker:
         h = np.round(np.random.normal(0, 1, self.n_spins), decimals=4)
 
         couplings = [h, J]
-        alpha = np.sqrt(self.n_spins) / np.sqrt(sum([J[i][j] ** 2 for i in range(self.n_spins) for j in range(i)]) + sum([h[j] ** 2 for j in range(self.n_spins)]))
-        self.model = EnergyModel(n=self.n_spins, couplings=couplings, name=self.name, alpha=alpha)
+        self.model = EnergyModel(n=self.n_spins, couplings=couplings, name=self.name)
 
-    def make_coarse_grained_ising(self):
+    def make_coarse_grained_ising(self, coarse_graining_number):
         # I define an energy model with the couplings and subgroups list explicitly specified in the parameters of EnergyModel object
         # This kind of initialization of an EnergyModel is required to run the CM.update() method for coarse graining
         shape_of_J = (self.n_spins, self.n_spins)
@@ -49,30 +51,6 @@ class ModelMaker:
         h = np.round(np.random.normal(0, 1, self.n_spins), decimals=4)
 
         couplings = [h, J]
-        alpha = np.sqrt(self.n_spins) / np.sqrt(sum([J[i][j] ** 2 for i in range(self.n_spins) for j in range(i)]) + sum([h[j] ** 2 for j in range(self.n_spins)]))
-        # subgroups = [[0, 1, 2], [0, 1, 3], [0, 1, 4]]  # [][[0, 1, 2], [3, 4, 5], [6]]  # Example of subgroups for coarse graining
-        subgroups = [
-            (0, 1, 2, 3, 4),
-            (0, 1, 2, 3, 5),
-            (0, 1, 2, 3, 6),
-            (0, 1, 2, 4, 5),
-            (0, 1, 2, 4, 6),
-            (0, 1, 2, 5, 6),
-            (0, 1, 3, 4, 5),
-            (0, 1, 3, 4, 6),
-            (0, 1, 3, 5, 6),
-            (0, 1, 4, 5, 6),
-            (0, 2, 3, 4, 5),
-            (0, 2, 3, 4, 6),
-            (0, 2, 3, 5, 6),
-            (0, 2, 4, 5, 6),
-            (0, 3, 4, 5, 6),
-            (1, 2, 3, 4, 5),
-            (1, 2, 3, 4, 6),
-            (1, 2, 3, 5, 6),
-            (1, 2, 4, 5, 6),
-            (1, 3, 4, 5, 6),
-            (2, 3, 4, 5, 6),
-        ]
-        self.model = EnergyModel(n=self.n_spins, couplings=couplings, name=self.name, alpha=alpha)
+        subgroups = list(itertools.combinations(range(self.n_spins), coarse_graining_number))
+        self.model = EnergyModel(n=self.n_spins, couplings=couplings, name=self.name)
         self.cg = CoarseGraining(n=self.n_spins, subgroups=subgroups)
