d1984b1527a00374adcb51d2589f4e214edf7620
Stuartferguson00 <99267485+Stuartferguson00@users.noreply.github.com>
2026-03-25 17:35:09 +0000

    refactor H_E graphs into notebook

--- src/qemcmc/utils/model_maker.py
+++ src/qemcmc/utils/model_maker.py
@@ -16,7 +16,7 @@ class ModelMaker:
     coupling tensors and initialises an :class:`EnergyModel` instance.
     """
 
-    def __init__(self, n_spins: int, model_type: str, name: str, coarse_graining_number: int = 1, cost_function_signs: list = [-1, -1]):
+    def __init__(self, n_spins: int, model_type: str, name: str, cost_function_signs: list = [-1, -1]):
         self.name = name
         self.n_spins = n_spins
         self.cost_function_signs = cost_function_signs or [-1, -1]
@@ -26,8 +26,6 @@ class ModelMaker:
 
         if model_type == "Fully Connected Ising":
             self.make_fully_connected_ising()
-        elif model_type == "Coarse Grained Ising":
-            self.make_coarse_grained_ising(coarse_graining_number)
         elif model_type == "1D Ising":
             self.make_1D_Ising()
         else:
@@ -45,18 +43,4 @@ class ModelMaker:
         couplings = [h, J]
         self.model = EnergyModel(n=self.n_spins, couplings=couplings, name=self.name)
 
-    def make_coarse_grained_ising(self, coarse_graining_number):
-        # I define an energy model with the couplings and subgroups list explicitly specified in the parameters of EnergyModel object
-        # This kind of initialization of an EnergyModel is required to run the CM.update() method for coarse graining
-        shape_of_J = (self.n_spins, self.n_spins)
-        J = np.round(np.random.normal(0, 1, shape_of_J), decimals=4)
-        J_tril = np.tril(J, -1)
-        J_triu = J_tril.transpose()
-        J = J_tril + J_triu
-
-        h = np.round(np.random.normal(0, 1, self.n_spins), decimals=4)
-
-        couplings = [h, J]
-        subgroups = list(itertools.combinations(range(self.n_spins), coarse_graining_number))
-        self.model = EnergyModel(n=self.n_spins, couplings=couplings, name=self.name)
-        self.cg = CoarseGraining(n=self.n_spins, subgroups=subgroups)
+    
